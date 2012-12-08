beb9e9eab84195cad2cf11bde0ce77bf686086a3
turkcode <info@devanhcrow.com>
2012-12-07 22:33:53 -0600

    Update whois-parser.php

--- whois-parser.php
+++ whois-parser.php
@@ -2,9 +2,9 @@
   
   $domain = 'google.com';
   $key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; // get your API key from http://whoisparser.com
-	$host = "http://whoisparser.com/users/api/?key=$key&domain=$domain";
-	$whoisData = file_get_contents($host);
-	$results = json_decode($whoisData);
-	print_r($results);
+  $host = "http://whoisparser.com/users/api/?key=$key&domain=$domain";
+  $whoisData = file_get_contents($host);
+  $results = json_decode($whoisData);
+  print_r($results);
 
 ?>
